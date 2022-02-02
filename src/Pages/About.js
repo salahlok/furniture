af569a4b7cbd30ca06622662da478854a40eec2c
Lokdai Salah Eddine <salahlok@gmail.com>
2022-02-02 18:11:48 +0100

    fixed more spelling mistakes

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -20,7 +20,7 @@ const About = () => {
           <p>-I used <span>react redux</span> to make my website code clean</p>
           <p>-By defining <span>Reducers</span>,<span>Actions</span>,<span>Store</span> away from components.</p>
           <p>-And Calling it by <span>State</span> and <span>Dispatch</span> inside each component.</p>
-          <p>-Besite <span>react redux</span>,I used react Hooks <span>UseState</span> and <span>UseEffect</span> to display data</p>
+          <p>-Beside <span>react redux</span>,I used react Hooks <span>UseState</span> and <span>UseEffect</span> to display data</p>
           <p>-For <span>CSS</span> I used both <span>FlexBox</span> and <span>Grid</span> To make my website look clean and beautiful on each Device.</p>
           <p>-I brought all data from Amazone website and did not used <span>APIs</span> I have enough knowledge to implement data from <span>APIs</span>, I can implement <span>PayPal</span> payments as well.</p>
           <p>-For more info about my services do not hesitate to contact me , This website is fully client side only.</p>
